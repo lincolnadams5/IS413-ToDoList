1c2cdcf1b905db2a914f3e62322ad053048c2cf5
Lincoln Adams <lincolnadams@icloud.com>
2026-02-27 18:08:59 -0700

    Fixed missing requirements, connected views to controller/model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using IS413_ToDoList.Models;
 using Task = IS413_ToDoList.Models.Task;
 
@@ -27,6 +28,9 @@ public class HomeController : Controller
 
     public IActionResult AddEditTask(int? id)
     {
+        ViewBag.Categories = _repo.GetAllCategories()
+            .Select(c => new SelectListItem { Value = c.CategoryID.ToString(), Text = c.CategoryName });
+
         var task = new Models.Task();
         if (id == null)
         {
@@ -37,6 +41,35 @@ public class HomeController : Controller
             task = _repo.GetTaskById(id.Value);
             return View(task);
         }
+    }
+
+    [HttpPost]
+    public IActionResult AddEditTask(Task task)
+    {
+        if (task.TaskId == 0)
+            _repo.AddTask(task);
+        else
+            _repo.UpdateTask(task);
+
+        TempData["Success"] = "Task saved successfully!";
+        return RedirectToAction("Quadrants");
+    }
+
+    public IActionResult MarkComplete(int id)
+    {
+        var task = _repo.GetTaskById(id);
+        if (task != null)
+        {
+            task.Completed = true;
+            _repo.UpdateTask(task);
+        }
+        return RedirectToAction("Quadrants");
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _repo.DeleteTask(id);
+        return RedirectToAction("Quadrants");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
